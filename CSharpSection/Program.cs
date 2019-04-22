fa66b554061d064177fd9ce02e7c525894eeef93
alfredo.yap <alfredo.yap@stu.bmcc.cuny.edu>
2019-04-22 19:21:39 -0400

    csharp fundamentals

--- CSharpSection/Program.cs
+++ CSharpSection/Program.cs
@@ -8,8 +8,60 @@ namespace CSharpSection
 {
     class Program
     {
-        static void Main(string[] args)
+        static int n1 = 432, n2 = 543;
+        private static void Main(string[] args)
         {
+            Console.WriteLine(Add());
+            Console.WriteLine(Subtract());
+            Console.WriteLine(Divide());
+            Console.WriteLine(Multiply());
+
+            Console.ReadLine();
         }
+
+        private static int Add()
+        {
+            int result;
+            result = n1 + n2;
+
+            return result;
+        }
+
+        private static int Subtract()
+        {
+            int result;
+            if (n1 > n2)
+            {
+                result = n1 - n2;
+            }
+            else
+            {
+                result = n2 - n1;
+            }            
+            return result;
+        }
+
+        private static int Divide()
+        {
+            int result;
+            if (n1 > n2)
+            {
+                result = n1 / n2;
+            }
+            else
+            {
+                result = n2 / n1;
+            }
+            return result;
+        }
+
+        private static int Multiply()
+        {
+            int result;
+            result = n1 * n2;
+            return result;
+        }
+
+
     }
 }
