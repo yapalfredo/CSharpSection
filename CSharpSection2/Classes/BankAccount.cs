9d5cb19e9a30fa1293d4495dfaaef5953702767c
alfredo.yap <alfredo.yap@stu.bmcc.cuny.edu>
2019-04-22 21:16:29 -0400

    C# fundamentals tutorials completed

---
+++ CSharpSection2/Classes/BankAccount.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpSection2.Classes
+{
+    public class BankAccount
+    {
+        private float balance;
+        private string owner;
+
+        //no need
+        //public BankAccount()
+        //{
+        //    balance = 0;
+        //    owner = "";
+        //}
+
+        //PROPERTY
+        public float Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            //setter has to be private
+            private set
+            {
+                if (value>=0)
+                {
+                    balance = value;
+                }
+                else
+                {
+                    balance = 0;
+                }
+                
+            }
+        }
+        //PROPERTY
+        public string Owner
+        {
+            get
+            {
+                return owner;
+            }
+
+            //setter has to be private
+            private set
+            {
+                owner = value;
+            }
+        }
+
+        public BankAccount(float balance, string owner)
+        {
+            Balance = balance;
+            Owner = owner;
+        }
+
+        //set virtual for overloading from child
+        public virtual float AddBalance(float balanceToBeAdded)
+        {
+            Balance = balance + balanceToBeAdded;
+            return Balance;
+        }
+
+        public virtual float AddBalance(float balanceToBeAdded, bool balanceCanBeNegative)
+
+        {
+            if (balanceCanBeNegative)
+            {
+                balance = balance + balanceToBeAdded;
+            }
+            else
+            {
+                Balance = balance + balanceToBeAdded;
+            }
+
+            return Balance;
+        }
+        
+        //asynchronus programming
+        //this simulates fetching data from internet
+        //in reality, you wouldn't know how long it would take
+
+        //use '<data type>' if you want to return something
+        public async Task<string> GetData()
+        {
+            Thread.Sleep(5000);
+            return "Completed";
+        }
+
+    }
+    
+    //inheritance
+    public class ChildBankAccount : BankAccount
+    {
+        public string Parent { get; set; }
+
+        public ChildBankAccount(float balance, string owner, string parent) : base(balance, owner)
+        {
+            Parent = parent;
+        }
+
+        public override float AddBalance(float balanceToBeAdded)
+        {
+            if(balanceToBeAdded >= -10)
+            {
+                return base.AddBalance(balanceToBeAdded);
+            }
+            else
+            {
+                return Balance;
+            }
+        }
+
+        public override float AddBalance(float balanceToBeAdded, bool balanceCanBeNegative)
+        {
+            if (balanceToBeAdded >= -10)
+            {
+                return base.AddBalance(balanceToBeAdded, balanceCanBeNegative);
+            }
+            else
+            {
+                return Balance;
+            }
+            
+        }
+    }
+
+}
---
+++ CSharpSection2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpSection2.Classes;
+
+namespace CSharpSection2
+{
+    class Program
+    {
+        static IOperations am;
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(SimpleMath.Add(123.11f, 134.34f));
+            //Console.WriteLine(SimpleMath.Subtract(123.11f, 134.34f));
+            //Console.WriteLine(SimpleMath.Multiply(123.11f, 134.34f));
+            //Console.WriteLine(SimpleMath.Divide(123.11f, 134.34f));
+            /*
+            am = new AdvancedMath();
+            Console.WriteLine(am.Remainder(7, 3));*/
+
+            /*
+            BankAccount bankAccount1 = new BankAccount(13235.23f, "Jane Doe");
+            ChildBankAccount bankAccount2 = new ChildBankAccount(2354.12f, "John Doe", "Jane Doe");
+
+            Console.WriteLine(bankAccount1.Owner +"'s balance is: " + bankAccount1.AddBalance(100f));
+            Console.WriteLine(bankAccount2.Owner +"'s balance is: " + bankAccount2.AddBalance(-3100f,true));
+            */
+            GetData();
+            Console.ReadLine();
+        }
+
+        //make it async
+        async static void GetData()
+        {
+            BankAccount bankAccount = new BankAccount(245456546, "Uncle Sam");
+
+            Console.WriteLine("Log in");
+         //   await bankAccount.GetData();
+            var task = await bankAccount.GetData();
+            Console.WriteLine("Display data");
+            Console.WriteLine(task);
+        }
+    }
+
+    interface IOperations
+    {
+        float Remainder(float dividend, float divisor);
+
+    }
+
+    class SimpleMath
+    {
+        public static float Add(float n1, float n2)
+        {
+            return n1 + n2;
+        }
+
+        public static float Subtract(float n1, float n2)
+        {
+            return n1 - n2;
+        }
+
+        public static float Multiply(float n1, float n2)
+        {
+            return n1 * n2;
+        }
+
+        public static float Divide(float n1, float n2)
+        {
+            return n1 / n2;
+        }
+    }
+
+    class AdvancedMath : SimpleMath, IOperations
+    {
+        public float Remainder(float dividend, float divisor)
+        {
+            return dividend % divisor;
+        }
+    }
+}
